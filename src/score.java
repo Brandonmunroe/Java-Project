2353b7848021a6145a0f1ae1afed16fd3a116eff
Brandonmunroe <157241940+Brandonmunroe@users.noreply.github.com>
2025-11-05 16:41:03 -0500

    Exam Score entry

---
+++ src/score.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class score
+{
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+
+
+        System.out.println("Please Enter exam score");
+
+        int score = input.nextInt();
+
+
+
+        if (score >= 90) {
+            System.out.println("Grade A");
+        } else if (score >= 80) {
+            System.out.println("Grade B");
+        } else if (score >= 70) {
+            System.out.println("Grade C");
+        } else if (score >= 60) {
+            System.out.println("Grade D");
+        } else {
+            System.out.println("Grade F");
+        }
+    }
+}
